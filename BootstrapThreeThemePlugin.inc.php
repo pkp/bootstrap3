130c5d436cb70ea488a703cbeed3df4eef82054f
Nate Wright <natew@notthisway.com>
2016-08-21 18:24:03 +0100

    Load jQuery

--- BootstrapThreeThemePlugin.inc.php
+++ BootstrapThreeThemePlugin.inc.php
@@ -23,6 +23,25 @@ class BootstrapThreeThemePlugin extends ThemePlugin {
 	 */
 	public function init($themes) {
 		$this->addStyle('bootstrap', 'styles/bootstrap.less');
+
+		// Load jQuery from a CDN or, if CDNs are disabled, from a local copy.
+		$min = Config::getVar('general', 'enable_minified') ? '.min' : '';
+		$request = Application::getRequest();
+		if (Config::getVar('general', 'enable_cdn')) {
+			$jquery = '//ajax.googleapis.com/ajax/libs/jquery/' . CDN_JQUERY_VERSION . '/jquery' . $min . '.js';
+			$jqueryUI = '//ajax.googleapis.com/ajax/libs/jqueryui/' . CDN_JQUERY_UI_VERSION . '/jquery-ui' . $min . '.js';
+		} else {
+			// Use OJS's built-in jQuery files
+			$jquery = $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jquery/jquery' . $min . '.js';
+			$jqueryUI = $request->getBaseUrl() . '/lib/pkp/lib/vendor/components/jqueryui/jquery-ui' . $min . '.js';
+		}
+		// Use an empty `baseUrl` argument to prevent the theme from looking for
+		// the files within the theme directory
+		$this->addScript('jQuery', $jquery, array('baseUrl' => ''));
+		$this->addScript('jQueryUI', $jqueryUI, array('baseUrl' => ''));
+		$this->addScript('jQueryTagIt', $request->getBaseUrl() . '/lib/pkp/js/lib/jquery/plugins/jquery.tag-it.js', array('baseUrl' => ''));
+
+		// Load Boostrap
 		$this->addScript('bootstrap', 'bootstrap/js/bootstrap.min.js');
 	}
 
